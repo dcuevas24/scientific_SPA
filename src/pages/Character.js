ae05f1ca668f86748f14c5e4b68d6e81775af80e
dcuevas24 <ivandaniel1224@gmail.com>
2022-04-08 11:41:35 -0500

    CSS styles

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -6,11 +6,11 @@ const Character = async () => {
   const character = await getData(id);
   const view = `
     <div class="Characters-inner">
-      <article class="Character-card">
+      <article class="Characters-card">
         <img src="${character.image}" alt="${character.name}">
         <h2>${character.name}</h2>
       </article>
-      <article class="Character-card">
+      <article class="Characters-card">
         <h3>Episodes:<span>${character.episode.length}</span></h3>
         <h3>Status:<span>${character.status}</span></h3>
         <h3>Species:<span>${character.species}</span></h3>
